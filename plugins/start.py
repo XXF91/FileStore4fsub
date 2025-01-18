7130615ff7f5c5ad659ed88d7574430e05bc0282
rohit3607 <rohitplayer87089@gmail.com>
2025-01-18 16:38:17 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -127,8 +127,9 @@ async def not_joined(client: Client, message: Message):
     except IndexError:
         pass
 
-    await message.reply(
-        text=FORCE_MSG.format(
+    await message.reply_photo(
+        photo=FORCE_PIC,
+        caption=FORCE_MSG.format(
             first=message.from_user.first_name,
             last=message.from_user.last_name,
             username=None if not message.from_user.username else '@' + message.from_user.username,
